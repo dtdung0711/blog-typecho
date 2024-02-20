c8c68641360de2bfff3931dfaddf0a2e113e6a1f
vitocorleone <dtdung0711@gmail.com>
2024-02-20 21:20:42 +0700

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -36,12 +36,12 @@ Typecho_Common::init();
 /** 定义数据库参数 */
 $db = new Typecho_Db('Pdo_Mysql', 'typecho_');
 $db->addServer(array (
-  'host' => '数据库地址',
-  'user' => '数据库用户',
-  'password' => '数据库密码',
+  'host' => 'monorail.proxy.rlwy.net',
+  'user' => 'root',
+  'password' => 'BEB522D1DdG1cdb3eH3ac1H6fbEee61e',
   'charset' => 'utf8mb4',
   'port' => '3306',
-  'database' => '数据库名',
+  'database' => 'railway',
   'engine' => 'MyISAM',
 ), Typecho_Db::READ | Typecho_Db::WRITE);
 Typecho_Db::set($db);
